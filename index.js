a7b5a41ef3e4c7d293de75ab4e904cceb5e01167
rdugas23 <glowingshark75@gmail.com>
2020-08-05 22:41:02 -0400

    another commti hee

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Master Page file called data.txt
-//Write new notes to disk every 5 mins
+//Write new notes to disk every 1 min
 //Cache changes in list and then clear to disk on write to disk queue
 
 //Serve Client Page
@@ -22,9 +22,51 @@ app.listen(port, function() {
 });
 
 app.get('/', function(req, res){
-  res.sendFile('index.html', { root: __dirname + "/" } );
+    res.writeHead(200, {"Content-Type": "text/html"});
+    var desFile = fs.readFileSync('index.txt').toString();
+    var fileArray = desFile.split('\n');
+    for(var i=0; i<fileArray.length; i++){
+      res.write(fileArray[i]);
+      if(i == 7){
+        res.write('<a href="#">' + queries[contentList[0]] + '</a>');
+      }
+      //SEND A DIV WITH A SPECIAL ID CONTAINING THE SERVER INFORMATION. THE WEBSITE CAN THEN PROCESS THE INFORMATION IN THE DIV AND HIDE THE DIV.
+    }
+    res.end();
+});
+
+app.post('/indexAddition', function(req, res){
+    newData.push(req.body.text);
+    console.log("Text Recieved: " + req.body.text);
+    res.sendFile('clientResources/index.html', { root: __dirname + "/" } );
 });
 
 console.log("Service Initialized...");
 
-//Read disk file into list
+//Data management
+
+var rawData = fs.readFileSync('data/data.txt');
+var dataString = rawData.toString();
+var readData = dataString.split('\n');
+
+var newData = [];
+
+//Clocking
+
+setInterval(function(){
+    writeToDisk(newData);
+}, 60000);
+
+function writeToDisk(data){
+    console.log("Writing " + data.length + " Chunks to disk");
+    for(var i=0; i< data.length; i++){
+        console.log("Writing " + (i/data.length)*100 + "% of " + data.length + " Chunks completed");
+        fs.appendFileSync('data/data.txt', data[i] + '\n');
+    }
+    console.log("Write completed, clearing console in 15 sec");
+    setTimeout(function(){
+        console.clear();
+        console.log("System OK");
+    }, 15000);
+    
+}
